f531e1a560dd6d0aba230c9058b67576b5c46a2e
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-06 21:38:58 -0600

    turned off arrows

--- app/js/page.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(document).on('click', '.submission', function(){ // Click .submission - Arrow is visible
-    $('#left-arrow, #right-arrow').show()
-  })
-
-  $('nav').click(function(){ // Click nav - Arrow is hidden
-    $('#left-arrow, #right-arrow').hide()
-  })
-})
