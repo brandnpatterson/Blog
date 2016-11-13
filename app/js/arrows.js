705c619e39087577c8c5ade98f9f36e36d4490c6
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-13 15:52:58 -0600

    Put arrows back. Created logic for the counter and direction in arrows.js

--- app/js/arrows.js
+++ app/js/arrows.js
@@ -1,35 +1,72 @@
 var Arrows = (function() {
 
   // cacheDOM
-  var $arrowLeft = $('.left-arrow');
-  var $arrowRight = $('.right-arrow');
-  var $arrows = $('.left-arrow, .right-arrow');
-  var $document = $(document);
-  var $nav = $('nav');
-  var $home = $('#home');
+  var $arrowLeft  = $('.arrow-left');
+  var $arrowRight = $('.arrow-right');
+  var $arrows     = $('.arrow-left, .arrow-right');
+  var $home       = $('#home');
+  var $nav        = $('#nav');
+  var $number     = document.getElementById('number');
+  var value       = parseInt($number.value, 10);
+  var $doc        = $(document);
 
   // bind events
-  $arrowRight.click(pageId);
-  $arrows.click(arrows);
-  $document.on('click', '.submission', submission);
-  $nav.click(hide);
+  $arrowLeft.click(historyBack);
+  $arrowRight.click(incrementValue);
+  $doc.on('click', '.page1', page1);
+  $doc.on('click', '.page2', page2);
+  $doc.on('click', '.page3', page3);
+  $nav.click(reset, arrowLeftHide);
 
-  function submission(){
-    $arrows.show();
-  }
+  function incrementValue() {
+    value = isNaN(value) ? 0 : value;
+    value ++;
+    $number = value;
+    document.getElementById('number').value = value;
+    // set the href of the right-arrow to be '#page'+the value given, matching the hash with the next page
+    $arrowRight.attr('href', '#page'+value);
 
-  function hide(){
-    $arrows.hide();
-  }
-
-  function pageId() {
-    var pageId = $(this).attr('data-click');
-    $('#'+pageId+2).toggle();
-  }
-
-  function arrows() {
-    if($arrowLeft.attr('data-click') == 'page0') {
-      $arrows.attr('href') == '#about';
+    if (value >= 4) {
+      value = 0;
+      $arrowRight.attr('href', '#home');
     }
+    $arrowLeft.show();
+  }
+
+  function arrowLeftHide() {
+    $arrowLeft.hide();
+  }
+
+  function historyBack() {
+    history.back();
+    value --;
+    document.getElementById('number').value = value;
+    return false;
+  }
+
+  function page1() {
+    var page1Val = $('.page1').attr('href').substr(5);
+    value = page1Val;
+    document.getElementById('number').value = value;
+    $arrowLeft.show();
+  }
+
+  function page2() {
+    var page2Val = $('.page2').attr('href').substr(5);
+    value = page2Val;
+    document.getElementById('number').value = value;
+    $arrowLeft.show();
+  }
+
+  function page3() {
+    var page3Val = $('.page3').attr('href').substr(5);
+    value = page3Val;
+    document.getElementById('number').value = value;
+    $arrowLeft.show();
+  }
+
+  function reset() {
+    value = 0;
+    document.getElementById('number').value = value;
   }
 })();
