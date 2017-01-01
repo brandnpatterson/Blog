f081f18b2e6c527fae5b0db94e9fba811cd3b8d0
Brandon Patterson <brandonnpatterson@gmail.com>
2016-12-31 19:52:25 -0600

    added second blog post

--- app/includes/post2/post-body.php
+++ app/includes/post2/post-body.php
@@ -7,14 +7,12 @@
       In order to do this I've structured my files into a post system that is similar to Wordpress. Each post has a header and a body. On the "latest" page, only the page header is visible, which is exactly what I want. On the page body, the header is imported there before the body content, so all the content looks like it belongs there.
     </p>
     <p>
-      The header currently has a "Continue Reading" link, but I have yet to make it dissapear when clicked. The post body provides a back link, which works naturally.
+      Following the header, there is "continue.php" for each pos, which allows the Continue Reading link to be only included in the post header and not the post body. I was unable to use jQuery to simply hide or show the divs because they were dynamically included. I can imagine that there is a possible solution, but allowing the php file to be seperately included into the header allows exactly what I need.
     </p>
     <div class="divider"></div>
     <p>
-      This file structure feels scalable for what I'm doing and I'm looking forward to making it even better as I move forward with it. My next steps are to make the "Continue Reading" link dissapear on selection.
+      This file structure feels scalable for what I'm doing and I'm looking forward to making it even better as I move forward with it.
     </p>
-    <p>
-    I made it dissapear by dividing it into its own PHP file.</p>
     <a class="go-back" href="#latest">
       Go Back
     </a>
